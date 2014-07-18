3e2e1f52aa47b09628616d8a9876238cf5e6abe8
DREWCIFER <drewcifer@imacdifederico.lan>
2014-07-18 20:52:32 +0200

    Initial Commit

---
+++ dynttt.cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dynttt.cpp
+//
+//  Created by DREWCIFER on 18/07/14.
+//  Copyright (c) 2014 dmm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
